7e55dd4d011f1519242bdd4433a4eb7b4d097f01
Frauke Albrecht <albrecht@dkrz.de>
2023-01-12 22:32:33 +0100

    python test

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-x = 1
-y = 2
-z = 1 +2
-
-print("x + y = ", z)
-print("this is  test file")
-
---
+++ test_sample.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
